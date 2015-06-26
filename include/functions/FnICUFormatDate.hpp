1e0aad49252362bf90c41ae9e4354245938fdcff
Ivan E. Panchenko <ivan.e.panchenko@gmail.com>
2015-06-26 23:34:45 +0300

    forgot it

---
+++ include/functions/FnICUFormatDate.hpp
@@ -0,0 +1,40 @@
+#ifndef _FN_ICU_DATE_FORMAT_HPP__
+#define _FN_ICU_DATE_FORMAT_HPP__ 1
+#include "CTPP2SysHeaders.h"
+
+#ifdef ICU_SUPPORT
+
+#include <CDT.hpp>
+
+#include <CTPP2VMSyscall.hpp>
+#include <unicode/smpdtfmt.h>
+
+namespace CTPP
+{
+// Наш обработчик
+class FnICUFormatDate:
+  public CTPP::SyscallHandler
+{
+private:
+
+	SimpleDateFormat *parser;
+
+public:
+	FnICUFormatDate();
+	~FnICUFormatDate() throw();
+
+	// Обработчик
+	virtual INT_32 Handler(CTPP::CDT     * aArguments,
+	                       const UINT_32   iArgNum,
+	                       CTPP::CDT     & oCDTRetVal,
+	                       CTPP::Logger  & oLogger);
+
+	// Имя функции
+	virtual CCHAR_P GetName() const;
+};
+
+} // namespace 
+#endif // ICU_SUPPORT
+#endif // _FN_ICU_DATE_FORMAT_HPP__
+// End.
+
