e80fd6f1ceca1616151e6430e345542a746bb6b0
Ivan Panchenko <ivan.e.panchenko@gmail.com>
2016-07-14 20:56:02 +0300

    fix unicode date format

--- src/functions/FnICUFormatDate.cpp
+++ src/functions/FnICUFormatDate.cpp
@@ -31,42 +31,53 @@ INT_32 FnICUFormatDate::Handler(CTPP::CDT          * aArguments,
 {
 	UDate date;
 	// Наша функция принимает один или два агрумента
-	if (iArgNum != 2 && iArgNum !=1) { return -1; }
+	if (iArgNum != 2 && iArgNum !=1 && iArgNum !=4 ) { return -1; }
 	int iFormat;       // номер аргумента с форматной строкой (в CTPP аргументы считаются сзади)
+	Locale * locale = NULL;
 	if( iArgNum==1) {  // один аргумент - берем текущее время
 		iFormat = 0;
 		date = 1000* (double)time(NULL);
-	} else {           // 2 аргумента
-		iFormat = 1;
-		if(int v = aArguments[0].GetInt()) {
+	} else if(iArgNum==2 || iArgNum==4 ) {           // 2 или 4 аргумента
+		iFormat  = iArgNum-1;
+		int iVal = iArgNum-2;
+		if (iArgNum==4) { 
+			locale = new Locale(aArguments[1].GetString().c_str(), aArguments[0].GetString().c_str());
+		}
+		if(int v = aArguments[iVal].GetInt()) {
 			date = v * 1000;
-		
 		} else {
-			std::string isodate = aArguments[0].GetString();
+			std::string isodate = aArguments[iVal].GetString();
 // does not work in gc < 4.9 
 //			isodate = std::regex_replace (isodate,std::regex("\\.\\d+"),std::string(""));
 // so do it manually 
-                        size_t dot_pos = isodate.find('.');
-                        if( dot_pos != std::string::npos) { //found
-                                size_t last_digit;
-                                for( last_digit = dot_pos+1; last_digit < isodate.length(); last_digit++) { 
-                                        if(!isdigit(isodate[last_digit])) break;
-                                }
-                                isodate = isodate.substr(0,dot_pos) +  isodate.substr(last_digit);
-                        }
+ 			size_t dot_pos = isodate.find('.');
+			if( dot_pos != std::string::npos) { //found
+				size_t last_digit;
+				for( last_digit = dot_pos+1; last_digit < isodate.length(); last_digit++) { 
+					if(!isdigit(isodate[last_digit])) break;
+				}
+				isodate = isodate.substr(0,dot_pos) +  isodate.substr(last_digit);
+			}
 //
 			ParsePosition pp(0);
 			date = parser->parse(UnicodeString(isodate.c_str()), pp);
 		}
 	}
 	UErrorCode err = U_ZERO_ERROR;	
-	SimpleDateFormat formatter(UnicodeString(aArguments[iFormat].GetString().c_str())	, err);
-	
-	UnicodeString myus;
-	UnicodeString us = formatter.format (date, myus);
-	std::string s;
-	us.toUTF8String<std::string> (s) ;
-	oCDTRetVal = s.c_str();
+	if(locale) { 
+		SimpleDateFormat formatter(UnicodeString(aArguments[iFormat].GetString().c_str())	, *locale, err);
+		UnicodeString myus;
+		std::string s;
+		formatter.format (date, myus).toUTF8String<std::string> (s) ;
+		oCDTRetVal = s.c_str();
+		delete locale;
+	} else { 
+		SimpleDateFormat formatter(UnicodeString(aArguments[iFormat].GetString().c_str())	, err);
+		UnicodeString myus;
+		std::string s;
+		formatter.format (date, myus).toUTF8String<std::string> (s) ;
+		oCDTRetVal = s.c_str();
+	}
 	return 0;
 }
 
